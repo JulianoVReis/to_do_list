06b61c49f7895f4a9096ad5bc9da00476b789ee9
Juliano Reis <julianovreis@gmail.com>
2025-06-27 15:44:35 -0300

    update to do list

--- public/script.js
+++ public/script.js
@@ -1,5 +1,11 @@
 const body = document.querySelector('body')
 const addTarefa = document.querySelector('.adicionar-tarefa')
+const modal1 = document.querySelector('.modal-1')
+const modal2 = document.querySelector('.modal-2')
+const modal3 = document.querySelector('.modal-3')
+const modalCriar = document.querySelector('.modal-criar')
+const modalPendentesBox = document.querySelector('.modal-pendentes-box')
+const modalFinalizadasBox = document.querySelector('.modal-finalizadas-box')
 
 body.addEventListener('load', carregarTarefas)
 async function carregarTarefas() {
@@ -33,3 +39,20 @@ addTarefa.addEventListener('click', async () => {
 	})
 	carregarTarefas()
 })
+
+// Início modal abrir e fechar
+modal1.addEventListener('click', () => {
+	const estilo = getComputedStyle(modalCriar)
+	modalCriar.style.display = (estilo.display === 'none') ? 'flex' : 'none'
+})
+
+modal2.addEventListener('click', () => {
+	const estilo = getComputedStyle(modalPendentesBox)
+	modalPendentesBox.style.display = (estilo.display === 'none') ? 'flex' : 'none'
+})
+
+modal3.addEventListener('click', () => {
+	const estilo = getComputedStyle(modalFinalizadasBox)
+	modalFinalizadasBox.style.display = (estilo.display === 'none') ? 'flex' : 'none'
+})
+// Fim modal abrir e fechar
